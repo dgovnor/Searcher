e548af3df5a117992953dcdbf63aca5f64c4e508
dgovnor <billyjude007@gmail.com>
2019-12-21 19:23:22 +0100

    Updates

---
+++ precache-manifest.d33ff4082d77a121856b753d03d62c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab24c600a2c4958e6c1398966deae204",
+    "url": "/Searcher/index.html"
+  },
+  {
+    "revision": "b10feb28119eaaddb715",
+    "url": "/Searcher/static/css/main.02c2f72d.chunk.css"
+  },
+  {
+    "revision": "a9f548aef49709b47d89",
+    "url": "/Searcher/static/js/2.ad5db614.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/Searcher/static/js/2.ad5db614.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b10feb28119eaaddb715",
+    "url": "/Searcher/static/js/main.a48a7c04.chunk.js"
+  },
+  {
+    "revision": "29042c7c94f35e27b4ea",
+    "url": "/Searcher/static/js/runtime-main.5fa21a74.js"
+  }
+]);
